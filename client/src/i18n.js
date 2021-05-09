e36579505dced1191e05f5e3a8c9b8fff39e39ff
sahragty <omar_elsahragty@hotmail.com>
2021-05-09 09:20:41 +0200

    [EDIT] locales location

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,9 +3,6 @@ import Backend from "i18next-xhr-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import translationEN from "../public/locales/en/translation.json";
-import translationAR from "../public/locales/ar/translation.json";
-
 const fallbackLng = ["EN"];
 const availableLanguages = ["AR", "EN"];
 
@@ -32,10 +29,6 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      EN: { translation: translationEN },
-      AR: { translation: translationAR },
-    },
     fallbackLng,
     debug: false,
     whitelist: availableLanguages,
