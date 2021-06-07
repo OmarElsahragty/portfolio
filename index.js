b4017f40d8362729813daf3956a673056f6b113c
sahragty <omar_elsahragty@hotmail.com>
2021-06-08 00:26:24 +0200

    [ADD] sentry to backend

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import { error } from "consola";
-import Server from "./src/http/server";
-import Config from "./src/config";
+import { error, success } from "consola";
+import * as Sentry from "@sentry/node";
+import "@sentry/tracing";
+import main from "./main";
+
+console.success = (message) => {
+  success({
+    badge: true,
+    message,
+  });
+};
 
 console.error = (message) => {
   error({
@@ -9,8 +17,22 @@ console.error = (message) => {
   });
 };
 
-try {
-  new Server(Config.Port).start();
-} catch (err) {
-  console.error(err.message);
-}
+Sentry.init({
+  dsn: "https://77d00d9481ca410ab9aa57a61af68809@o783506.ingest.sentry.io/5805285",
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "Nodejs Backend",
+  name: "Sahragty's Portfolio",
+});
+
+setTimeout(() => {
+  try {
+    main();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
---
+++ main.js
@@ -0,0 +1,10 @@
+import Server from "./src/http/server";
+import Config from "./src/config";
+
+export default () => {
+  try {
+    new Server(Config.Port).start();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
