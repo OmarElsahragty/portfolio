9cfc63ecb1fca7c616ed9a341cbf73000d0bf082
sahragty <omar_elsahragty@hotmail.com>
2021-06-08 01:36:34 +0200

    Dummy

--- client/src/API/URLs.js
+++ client/src/API/URLs.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://sahragty.herokuapp.com/";
 
 /* -------------------------------------------------------------------------- */
 /*                               SERVICES                                     */
