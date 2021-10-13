13d79ef539f24b48eb97798b6956206dee50ee64
omar elsahragty <omar_elsahragty@hotmail.com>
2021-10-13 15:10:16 +0200

    maintenance

--- client/src/Components/Organisms/Footer/Footer.jsx
+++ client/src/Components/Organisms/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Tooltip } from "@material-ui/core";
 import { Code } from "@material-ui/icons";
+import { useTranslation } from "react-i18next";
 
 import { ImagesSrc } from "../../../utilities/Helpers";
 import { Copyrights } from "../../Atoms";
@@ -9,31 +10,35 @@ import Data from "../../../Data";
 
 import styles from "./Footer.module.css";
 
-const Footer = () => (
-  <Grid item className={styles.footer}>
-    <Grid
-      container
-      alignItems="center"
-      justify="space-between"
-      className={styles.container}
-    >
-      <Grid item>
-        <Avatar src={ImagesSrc.avatarThree} size={8.5} />
-      </Grid>
+const Footer = () => {
+  const { t } = useTranslation();
 
-      <Grid item>
-        <Copyrights />
-      </Grid>
+  return (
+    <Grid item className={styles.footer}>
+      <Grid
+        container
+        alignItems="center"
+        justify="space-between"
+        className={styles.container}
+      >
+        <Grid item>
+          <Avatar src={ImagesSrc.avatarThree} size={8.5} />
+        </Grid>
 
-      <Grid item>
-        <Tooltip title={t("Code")}>
-          <a target="_blank" rel="noreferrer github" href={Data.ProjectRepo}>
-            <Code classes={{ root: "social-icon" }} />
-          </a>
-        </Tooltip>
+        <Grid item>
+          <Copyrights />
+        </Grid>
+
+        <Grid item>
+          <Tooltip title={t("Code")}>
+            <a target="_blank" rel="noreferrer github" href={Data.ProjectRepo}>
+              <Code classes={{ root: "social-icon" }} />
+            </a>
+          </Tooltip>
+        </Grid>
       </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default Footer;
